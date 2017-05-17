786b34382af8ad1cdb012c30523d412e7ffd91ae
Freek Kalter <freek@kalteronline.org>
2017-05-17 19:44:10 +0200

    Optimized tmux setup

--- webapp/tmux-session.sh
+++ webapp/tmux-session.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 tmux new-window 'source ./myenv/bin/activate; python run.py'
-tmux new-window 'source ./myenv/bin/activate; celery -A prlib.celery worker -c 1'
+tmux split-window -v -t 2 'source ./myenv/bin/activate; celery -A prlib.celery worker -c 1'
 cd ./prlib/frontend/
-tmux split-window -v -t 2 'npm run build'
-tmux split-window -h
+tmux split-window -h 'npm run build'
 cd ../
