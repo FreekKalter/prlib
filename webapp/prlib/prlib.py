95dbeaa6db1146374e7787166376a35c1be2adb5
Freek Kalter <freek@kalteronline.org>
2017-11-09 22:23:10 +0100

    - improve random function to use Movie.chance - add comporess function to send tasks to compress-server

--- webapp/prlib/prlib.py
+++ webapp/prlib/prlib.py
@@ -5,12 +5,15 @@ from .declarative import Base, Movie, File, create
 from . import app
 
 import random
+import re
+import socket
 import subprocess
 from pathlib import Path
 from datetime import datetime
 
 
 engine = create_engine('sqlite:///' + app.config['DB_FILE'])
+compressed_regex = re.compile('-compressed\.mp4$')
 
 Base.metadata.bind = engine
 Session = sessionmaker(bind=engine, expire_on_commit=False)
@@ -19,8 +22,10 @@ Session = sessionmaker(bind=engine, expire_on_commit=False)
 def pick_random():
     global RANDOM_LIST
     global LAST_RANDOM
-    RANDOM_LIST.sort(key=lambda movie: movie.days, reverse=True)
-    total_change = sum([m.days for m in RANDOM_LIST])
+    if len(RANDOM_LIST) < 1:
+        RANDOM_LIST = all_movies()
+    RANDOM_LIST.sort(key=lambda movie: movie.change, reverse=True)
+    total_change = sum([m.change for m in RANDOM_LIST])
     if total_change == 0:
         choice = random.choice(RANDOM_LIST)
         LAST_RANDOM = choice.id
@@ -28,7 +33,7 @@ def pick_random():
     choice = random.randint(0, total_change)
     counter = 0
     for m in RANDOM_LIST:
-        counter = counter + m.days
+        counter = counter + m.change
         if counter >= choice:
             LAST_RANDOM = m.id
             m.last_played = datetime.now()
@@ -37,6 +42,47 @@ def pick_random():
             session.commit()
             session.close()
             return m
+
+
+def compress(rows):
+    files = all_files()
+    ids = [row['id'] for row in rows]
+    files = [file for file in files if file.movie_id in ids and not compressed_regex.search(file.location)]
+    session = Session()
+    for mId in [file.movie_id for fil in files]:
+        movie = get_movie(mId)
+        movie.comment += '_COMPRESSING_'
+        session.add(movie)
+    session.commit()
+    session.close()
+    locations = [file.location for file in files]
+    print(locations)
+    s = socket.socket()
+    s.connect(('localhost', 1234))
+    s.sendall(bytes('\n'.join(locations), 'UTF-8'))
+    s.shutdown(socket.SHUT_WR)
+    data = b''
+    while 1:
+        r = s.recv(1024)
+        if r == b'':
+            break
+        data += r
+    print(data.decode())
+    s.close()
+
+
+def tag_multiple(rows, tag):
+    session = Session()
+    for row in rows:
+        movie = get_movie(row['id'])
+        if movie.tags == '':
+            movie.tags = tag
+        else:
+            movie.tags += ',' + tag
+        session.add(movie)
+        print(movie)
+    session.commit()
+    session.close()
 
 
 def update_random_list(rows):
@@ -51,6 +97,13 @@ def all_movies():
     movies = session.query(Movie).options(joinedload(Movie.files)).all()
     session.close()
     return movies
+
+
+def all_files():
+    session = Session()
+    files = session.query(File).all()
+    session.close()
+    return files
 
 
 def get_nr_files_by_movie(id):
@@ -76,6 +129,8 @@ def delete_movies(ids):
 def delete_file(id):
     session = Session()
     file = session.query(File).filter(File.id == id).one()
+    session.delete(file)
+    session.commit()
     session.close()
     thumbnail = 'prlib/static/images/previews/' + file.thumbnail
     preview = 'prlib/static/images/previews/' + file.preview
@@ -121,6 +176,8 @@ def update_movie(id, movie_dict):
     session = Session()
     movie = session.query(Movie).filter(Movie.id == id).one()
     for key in movie_dict:
+        if key == 'files':
+            continue
         setattr(movie, key, movie_dict[key])
     session.add(movie)
     session.commit()
