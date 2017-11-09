a6f4de49a24bc71f4777a5d10fcd86b6299e0dca
Freek Kalter <freek@kalteronline.org>
2017-11-09 22:17:40 +0100

    - kill celery before starting new instance - watch task to restart clerey

--- webapp/tmux-session.sh
+++ webapp/tmux-session.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+ps ax| grep -v grep| grep "celery" -i | awk '{print $1}' | xargs kill
 tmux new-window 'source ./myenv/bin/activate; python run.py'
-tmux split-window -v -t 2 'source ./myenv/bin/activate; celery -A prlib.celery worker -c 1'
+tmux split-window -v -t 2 "source ./myenv/bin/activate;\
+                           watchmedo auto-restart -d ./prlib -p '*tasks.py' -- \
+                           celery -A prlib.celery worker -c 1 2>&1 | tee tasks.log"
 cd ./prlib/frontend/
 tmux split-window -h 'npm run build'
 tmux select-pane -t 0
